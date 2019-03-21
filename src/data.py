bf1dd45fec308aca05bfa91998ce800dd1aba8d6
Victoria Florence <vflorenc@umich.edu>
2019-03-21 16:01:09 -0400

    bug fix

--- src/data.py
+++ src/data.py
@@ -4,47 +4,64 @@ import numpy as np
 from english2phoneme import e2p
 
 class Dictionary(object):
-    def __init__(self):
-        self.word2idx = {}
-        self.idx2word = []
-        self.word2phonemes = {}
-        self.phoneme2idx = {}
-        self.idx2phoneme = []
+    def __init__(self, dict_path=None):
+        if dict_path:
+            self.word2phonemes, self.idx2phoneme = pickle.load(open(dict_path, 'rb'))
+            self.idx2word = self.word2phonemes.keys()
+            self.word2idx = {word:idx for idx,word in enumerate(self.idx2word)}
+            self.phoneme2idx = {phoneme:idx for idx,phoneme in enumerate(self.idx2phoneme)}
 
-    def __len__(self):
-        return len(self.idx2word)
+            #Convert dictionary to token form 
+            self.word2phonemes_idx = [[] for word in self.idx2word]
+            for word in self.word2phonemes:
+                self.word2phonemes_idx.append([])
+                for phoneme in self.word2phonemes[word]:
+                    self.word2phonemes_idx[self.word2idx[word]].append(self.phoneme2idx[phoneme])
+                    
+
+        else:
+            self.word2phonemes = {}
+            self.word2phonemes_idx = []
+            self.word2idx = {}     
+            self.idx2word = []  
+            self.phoneme2idx = {}
+            self.idx2phoneme = []
 
 class Corpus(object):
     def __init__(self, path):
-        self.dictionary = Dictionary()
-
-        # Tokenize lyrics 
-        lyric_path = os.path.join(path, 'lyrics.txt')
-        self.lyrics = self.tokenize(lyric_path)
-
         #Get or create phoneme dictionary if you recreate dictionary, you must recreate phonemes
-        dict_path = os.path.join(path, 'w2p.pickle')
+        lyric_path = os.path.join(path, 'lyrics.txt')
         phoneme_path = os.path.join(path, 'phonemes.txt')
+        dict_path = os.path.join(path, 'dict.pickle')
+
         if os.path.exists(dict_path):
-            self.dictionary.idx2phoneme, self.dictionary.phoneme2idx, self.dictionary.word2phonemes = pickle.load(open(dict_path, 'rb'))
-            if os.path.exists(phoneme_path):
-                self.phonemes = self.tokenize_phonemes(phoneme_path)
-            else:
-                self.phonemes = self.create_phonemes(phoneme_path)
+            self.dictionary = Dictionary(dict_path)
+            lyrics_list = self.process_lyrics(lyric_path)
+            self.lyrics = self.tokenize(lyrics_list)
+            self.phonemes = self.tokenize_phonemes(phoneme_path)            
         else:
-            self.create_pdicts(dict_path)
-            self.phonemes = self.create_phonemes(phoneme_path)       
-            
-    def tokenize(self, lyric_path, is_phonemes=False):
-        """Tokenizes a text file."""
-        assert os.path.exists(lyric_path)
+            self.dictionary = Dictionary()
+            self.create_dictionaries(lyric_path, phoneme_path, dict_path)
 
-        # Add words to the dictionary
-        f_lyric = re.sub(' {2,}', ' ', re.sub('[\n]',' \n ', open(lyric_path, 'r', encoding="utf8").read().strip())).split(' ')
-        self.dictionary.idx2word = list(set(f_lyric))
+    def create_dictionaries(self, lyric_path, phoneme_path, dict_path):
+        lyrics_list = self.process_lyrics(lyric_path)
+        self.dictionary.idx2word = list(set(lyrics_list))
         self.dictionary.word2idx = {word:idx for idx,word in enumerate(self.dictionary.idx2word)}
-        tokens = np.array([self.dictionary.word2idx[word] for word in f_lyric])
+        self.lyrics = self.tokenize(lyrics_list)
+        self.create_pdicts()
+        self.phonemes = self.create_phonemes(phoneme_path)
+        pickle.dump((self.dictionary.word2phonemes, self.dictionary.idx2phoneme), open(dict_path, 'wb'))
 
+    def process_lyrics(self, lyric_path):
+        f_lyric = open(lyric_path, 'r', encoding='utf8').read().strip()
+        lyrics_list = re.sub('[\n]',' \n ', f_lyric)
+        lyrics_list = re.sub(' {2,}', ' ', lyrics_list)
+        lyrics_list = lyrics_list.split(' ')
+        return lyrics_list
+
+    def tokenize(self, lyrics_list):
+        """Tokenizes a text file."""
+        tokens = np.array([self.dictionary.word2idx[word] for word in lyrics_list])     
         return tokens
     
     def tokenize_phonemes(self, phoneme_path):
@@ -61,52 +78,48 @@ class Corpus(object):
         return phonemes 
         
 
-    def create_pdicts(self, dict_path):
+    def create_pdicts(self):
         lyrics = ('<break>').join(self.dictionary.idx2word).lower()
-        num_chunks = math.ceil(len(lyrics) / 100000)
+        num_chunks = int(math.ceil(len(lyrics) / 100000.0))
         num_words = len(self.dictionary.idx2word)
         
         output = ''
         for i in range(num_chunks):
-            output += e2p(('<break>').join(self.dictionary.idx2word[i*num_words//num_chunks:min((i+1)*num_words//num_chunks, num_words)]).lower())
+            start_idx = i*num_words//num_chunks
+            stop_idx = min((i+1)*num_words//num_chunks, num_words)
+            words = self.dictionary.idx2word[start_idx:stop_idx]
+            output += e2p(('<break>').join(words))
         
         #Remove espeak extra symbols
         output = re.sub("[',%=_|;~]",'',output)
         output = re.sub("_:", '', output)
 
-        # A quirk of this is that the phoneme for the word \n is ''
+        # A quirk of the way we are using espeak is that the phoneme for the word \n is '', so we sub it with '\n'
         output = re.sub(' ', '-', output).strip('\n').split('\n')
         output[output.index('')] = '\n'
 
-        self.dictionary.idx2phoneme = list(set((('-').join(output).split('-'))))
+        word_list =  (('-').join(output)).split('-')
+        self.dictionary.idx2phoneme = list(set(word_list))
         self.dictionary.phoneme2idx = {phoneme:idx for idx,phoneme in enumerate(self.dictionary.idx2phoneme)}
 
-        w2p = [word.split('-') for word in output]
-        for widx, w in enumerate(w2p): 
+        w2p = {word:phonemes.split('-') for [word,phonemes] in zip(self.dictionary.idx2word, output)}
+        w2p_i = [word.split('-') for word in output]
+        for widx, w in enumerate(w2p_i): 
             for pidx, p in enumerate(w): 
-                w2p[widx][pidx] = self.dictionary.phoneme2idx[p]
+                w2p_i[widx][pidx] = self.dictionary.phoneme2idx[p]
         
         self.dictionary.word2phonemes = w2p
-
-        pickle.dump((self.dictionary.idx2phoneme, self.dictionary.phoneme2idx, self.dictionary.word2phonemes), open(dict_path,'wb'))
-
+        self.dictionary.word2phonemes_idx = w2p_i
         
     def create_phonemes(self, phoneme_path):
         phonemes=[]
-
-        for l in self.lyrics: 
-            phonemes.append(self.dictionary.word2phonemes[l]) 
-            
-        
         output = ''
-        for widx, w in enumerate(phonemes):
-            if widx != 0:
+        for lidx, l in enumerate(self.lyrics): 
+            phonemes.append(self.dictionary.word2phonemes_idx[l]) 
+            if lidx != 0:
                 output += ' '
-            for idx, p in enumerate(w):
-                if idx != 0:
-                    output+= '-'
-                output+=self.dictionary.idx2phoneme[p]
-            
+            output+= ('-').join(self.dictionary.word2phonemes[self.dictionary.idx2word[l]])
+
         #Write to file
         output_file = open(phoneme_path, 'w+', encoding='utf8')
         output_file.write(output)
